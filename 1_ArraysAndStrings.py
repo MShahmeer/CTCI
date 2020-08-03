8ce30841d437324264cca6dd1ece5c3d15f54e57
shahmeerathar <shahmeer.athar@gmail.com>
2020-08-03 19:55:24 -0400

    Completed Arrays and Strings Problem 3

--- 1_ArraysAndStrings.py
+++ 1_ArraysAndStrings.py
@@ -61,6 +61,26 @@ def check_permutations(s1: str, s2: str) -> bool:
 # Time complexity: O(n), space complexity: O(n)
 
 
+"""
+Problem 3 - URLify:
+Write a method to replace all spaces in a string with '%20: You may assume that
+the string has sufficient space at the end to hold the additional characters,
+and that you are given the "true" length of the string.
+"""
+
+
+def urlify(s: list, true_length: int) -> None:
+    offset = len(s) - true_length
+    for i in range(true_length - 1, -1, -1):
+        if s[i] != ' ':
+            s[i + offset] = s[i]
+        else:
+            s[i + offset - 2] = '%'
+            s[i + offset - 1] = '2'
+            s[i + offset] = '0'
+            offset = offset - 2
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
