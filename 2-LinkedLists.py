17319129c389a41a4610573b8ffafd7a482af12b
shahmeerathar <shahmeer.athar@gmail.com>
2020-08-21 19:55:20 -0400

    ~

--- 2-LinkedLists.py
+++ 2-LinkedLists.py
@@ -1,22 +1,8 @@
 """
 DOCTESTS:
-
-Problem 8:
->>> lst = LinkedList()
->>> node1 = _Node("A")
->>> node2 = _Node("B")
->>> node3 = _Node("C")
->>> node4 = _Node("D")
->>> node5 = _Node("E")
->>> node5.next = node3
->>> node4.next = node5
->>> node3.next = node4
->>> node2.next = node3
->>> node1.next = node2
->>> lst._first = node1
->>> loop_detection(lst)
-Node3
 """
+
+from DataStructures import _Node, LinkedList
 
 """
 Problem 8 - Loop Detection:
@@ -26,8 +12,6 @@ the beginning of the loop.
 Time complexity: O(n), space complexity: O(n)
 """
 
-from DataStructures import _Node, LinkedList
-
 
 def loop_detection(lst: LinkedList) -> _Node:
     encountered = []
@@ -36,18 +20,3 @@ def loop_detection(lst: LinkedList) -> _Node:
         encountered.append(curr)
         curr = curr.next
     return curr
-
-
-lst = LinkedList()
-node1 = _Node("A")
-node2 = _Node("B")
-node3 = _Node("C")
-node4 = _Node("D")
-node5 = _Node("E")
-node5.next = node3
-node4.next = node5
-node3.next = node4
-node2.next = node3
-node1.next = node2
-lst._first = node1
-print(loop_detection(lst).item)
